d12803b77eb9f222a9c85734a38ffaca0b65f8d0
edgarglezd <edgarg2098@gmail.com>
2022-03-15 15:47:22 -0600

    Creando mas rutas

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
+from routes.user import user
 
 app = FastAPI()
+
+@app.get('/')
+def get_raiz():
+    return {"Holiii aqui no hay ni %$& dirigete a /docs"}
+
+app.include_router(user)
